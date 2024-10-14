7f20b3f064be48778f69d57bbd9a96ded5f5cf35
elenamoralesgil <elena.morales104@alu.ulpgc.es>
2024-10-14 18:43:08 +0100

    Codelab Administrador de tareas completado

--- TaskAdministrator/app/src/main/java/com/example/taskadministrator/ui/theme/Theme.kt
+++ TaskAdministrator/app/src/main/java/com/example/taskadministrator/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
-package com.example.compose_article.ui.theme
+package com.example.taskadministrator.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -33,24 +34,25 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Compose_articleTheme(
+fun TaskAdministratorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
 
     MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
     )
 }
